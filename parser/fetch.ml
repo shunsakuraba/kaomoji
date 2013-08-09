eb59446ec93262f00e110f2014d7149e3a3fcf83
Takeshi Yoshino <ysnysnysn@gmail.com>
2013-08-09 16:44:19 +0900

    Reorg

--- parser/fetch.ml
+++ parser/fetch.ml
@@ -1,8 +1,7 @@
-let prefix = "http://icfpc2013.cloudapp.net"
-let auth = "AUTHKEYvpsH1H"
+open Api
 
 let fetch () =
-  let problem_url = prefix ^ "/myproblems?auth=" ^ auth in
+  let problem_url = api_site ^ "/myproblems?auth=" ^ auth in
   Http_user_agent.get problem_url
 ;;
 
