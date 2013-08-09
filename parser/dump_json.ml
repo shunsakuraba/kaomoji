a321c208c3d63fd0e0265e38f8823dbaf6d35d56
Tsutomu Ohkura <tsutomu@ohkura.com>
2013-08-09 17:41:31 +0900

    Show the # of solved problems and only show the unsolved problem in the list.

--- parser/dump_json.ml
+++ parser/dump_json.ml
@@ -1,7 +1,12 @@
 open Api;;
 
+(* tuple format *)
+(* id, size, operators, solved, timeleft *)
+
 let problem_json = Yojson.Safe.from_channel stdin in
 let problems = parse_problems_json problem_json in
+let num_solved = List.length(List.filter (fun (_, _, _, solved, _) -> solved) problems) in
+let not_solved = List.filter (fun (_, _, _, solved, _) -> not solved) problems in
 let sorted_problems =
   List.sort
     (fun (_, x_size, (_, _, x_s), _, _) (_, y_size, (_, _, y_s), _, _) ->
@@ -9,7 +14,10 @@ let sorted_problems =
       if l_diff != 0
       then l_diff
       else x_size - y_size)
-    problems in
+    not_solved in
+let _ = print_endline
+  (Printf.sprintf "Solved: %4d/%4d" num_solved
+     (List.length problems)) in
 List.iteri
   (fun i x -> print_problem x i)
   sorted_problems
