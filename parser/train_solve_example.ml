8d4e3c6a431273f0ae71733da6ebdd5b0716e43a
Takeshi Yoshino <ysnysnysn@gmail.com>
2013-08-09 17:39:36 +0900

    hugaa

--- parser/train_solve_example.ml
+++ parser/train_solve_example.ml
@@ -2,4 +2,6 @@ open Api;;
 
 let train_string = fetch_train 3 "fold" in
 let train = parse_train_string train_string in
-print_endline (format_train train 0)
+print_endline (format_train train 0);
+let id, size, (unops, binops, statements), challenge = train in
+()
