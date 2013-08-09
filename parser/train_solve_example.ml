43b3ab7cc5686e6778ccba9cfe823822f7d12cfc
Tsutomu Ohkura <tsutomu@ohkura.com>
2013-08-09 18:21:48 +0900

    Add train_guess_example.

--- parser/train_solve_example.ml
+++ parser/train_solve_example.ml
@@ -4,4 +4,7 @@ let train_string = fetch_train 3 "fold" in
 let train = parse_train_string train_string in
 print_endline (format_train train 0);
 let id, size, (unops, binops, statements), challenge = train in
-eval_id id [0; 100; 10000; 1000000; 100000000]
+let guess_result_string = guess id challenge in
+let guess_result = parse_guess_result_string guess_result_string in
+let (status, _, message, _)= guess_result in
+print_endline (Printf.sprintf "%s %s" status message)
