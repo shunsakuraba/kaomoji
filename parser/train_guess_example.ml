55c1dc98ce121790de5de185f1a67813f19c063d
Takeshi Yoshino <ysnysnysn@gmail.com>
2013-08-09 18:34:16 +0900

    hugas

--- parser/train_guess_example.ml
+++ parser/train_guess_example.ml
@@ -4,7 +4,6 @@ let train_string = fetch_train 3 "fold" in
 let train = parse_train_string train_string in
 print_endline (format_train train 0);
 let id, size, (unops, binops, statements), challenge = train in
-let guess_result_string = guess id challenge in
-let guess_result = parse_guess_result_string guess_result_string in
+let guess_result = guess id challenge in
 let (status, _, message, _)= guess_result in
 print_endline (Printf.sprintf "%s %s" status message)
--- parser/train_solve_example.ml
+++ parser/train_solve_example.ml
@@ -10,10 +10,9 @@ let _ = eval_id id [0L; 100L; 10000L; 1000000L; 100000000L; 0xffffffffffffffffL]
 let _ = eval_program "(lambda (x) (not 1))" [0L; 100L; 10000L; 1000000L; 100000000L] in
 let _ = guess id "(lambda (x) (not 1))" in
 ()
-;;
+;
 
-let id, size, (unops, binops, statements), challenge = train in
-let guess_result_string = guess id challenge in
-let guess_result = parse_guess_result_string guess_result_string in
-let (status, _, message, _)= guess_result in
+let id, _, _, challenge = train in
+let guess_result = guess id challenge in
+let status, _, message, _ = guess_result in
 print_endline (Printf.sprintf "%s %s" status message)
