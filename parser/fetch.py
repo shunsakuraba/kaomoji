b0b41b74a6f71001429323ad6aaab6332b83e50c
Takeshi Yoshino <ysnysnysn@gmail.com>
2013-08-09 17:12:09 +0900

    rename

--- parser/fetch.py
+++
@@ -1,8 +0,0 @@
-import urllib2
-
-SITE='http://icfpc2013.cloudapp.net/'
-AUTH='AUTHKEYvpsH1H'
-
-if __name__ == '__main__':
-    f = urllib2.urlopen(SITE + '/myproblems?auth=' + AUTH)
-    print f.read()
