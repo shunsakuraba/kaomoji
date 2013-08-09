f3887ebd1527e43c45c682d52cbfa0af3121d062
Tsutomu Ohkura <tsutomu@ohkura.com>
2013-08-10 03:10:32 +0900

    Generate candidate list before sending eval_id request.  It helps us to notice the stack limitation before opening the problem.

--- bruteforce/guessCaller.mli
+++ bruteforce/guessCaller.mli
@@ -1,7 +1,8 @@
-
-
-
 val guess_call: (Int64.t * Int64.t) list -> 
   (Type.expr -> Feedback.feedback) ->
-  (Type.unop list * Type.binop list * Type.statements list) -> 
-  int -> unit
+  (Type.unop list * Type.binop list * Type.statements list) ->
+  int ->
+  (Type.expr list) ->
+  unit
+
+
