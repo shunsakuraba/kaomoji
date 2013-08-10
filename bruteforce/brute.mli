c75612089c26d2f1fe27d8f1bfe66576e96c421b
Tsutomu Ohkura <tsutomu@ohkura.com>
2013-08-11 02:29:41 +0900

    Move simplifier code to simplifier.ml.

--- bruteforce/brute.mli
+++ bruteforce/brute.mli
@@ -1,3 +1,2 @@
 val gen: (Type.unop list * Type.binop list * Type.statements list) -> int -> Type.expr list
 val gen2: (Type.unop list * Type.binop list * Type.statements list) -> int -> Type.expr list
-val simplify: Type.expr -> Type.expr
--- bruteforce/test_brute.ml
+++ bruteforce/test_brute.ml
@@ -1,19 +1,26 @@
+open ExtList
+
 let print_candidate candidate =
-  if false then
+  if true then
     print_endline (Print.print candidate)
   else
     ()
 
 let main = 
-  let core_problem = Remote.fetch_one_core_problem 12 "" "train_local" in
+  (* AcSmrDU445Uu5ewnrBZ3rn9N*)
+  (* Yj5nGIfonf3VCIiAn2pEARdw *)
+  let core_problem = Remote.fetch_one_core_problem 8 "" "train_local" in
   let () = print_endline (Remote.format_core_problem core_problem) in
   let id, size, (unops, binops, statements) = core_problem in
   let cand_gen_start_time = Sys.time() in
   let allowed = (unops, binops, statements) in
-  let alllist = Brute.gen allowed size in
+  let alllist_initial = Brute.gen allowed size in
+  let alllist = List.unique (List.map Simplifier.simplify alllist_initial) in
   let _ = List.iter print_candidate alllist in
   let num_candidates = List.length alllist in
-  let () = Printf.printf "Initialized candidate list (%d elements)\n" num_candidates in
+  let () = Printf.printf "Initialized candidate list (%d elements)\n"
+  (List.length alllist_initial) in
+  let () = Printf.printf "Compressed candidate list (%d elements)\n" num_candidates in
   let start_time = Sys.time() in
   let cand_gen_time = start_time -. cand_gen_start_time in
   Printf.printf "Candidate generation time: %fs\n" cand_gen_time
