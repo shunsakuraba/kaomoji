e5b2a92ac5b71cabaf98043d4822a848250437ff
Tsutomu Ohkura <tsutomu@ohkura.com>
2013-08-10 16:25:50 +0900

    Suppress some of the duplicated Op1 subtress.

--- bruteforce/brute.ml
+++ bruteforce/brute.ml
@@ -62,6 +62,13 @@ let needed_depth bits =
     (((bits lsr 10) land 1) +
         ((bits lsr 11) land 1)) * 2
 
+let is_good_unop_cand cand =
+  match cand with
+  | Op1 (Shr1, Op1 (Shr4, _)) -> false
+  | Op1 (Shr4, Op1 (Shr16, _)) -> false
+  | Op1 (Shr1, Op1 (Shr16, _)) -> false
+  | _ -> true
+
 let gen (allowed_un, allowed_bin, allowed_stmts) depth =
   prerr_endline "Generating";
   let if0_in_ops = List.mem SIf0 allowed_stmts in
@@ -133,7 +140,9 @@ let gen (allowed_un, allowed_bin, allowed_stmts) depth =
 	  let cands = ref [] in
 	  List.iter (fun op ->
 	    List.iter (fun (x, x_used) ->
-	      cands := (Op1 (op, x), x_used lor (unused_un_bit op)) :: !cands)
+              let cand_exp = Op1 (op, x) in
+              if (is_good_unop_cand cand_exp) then
+		cands := (cand_exp, x_used lor (unused_un_bit op))  :: !cands)
 	      (gen input_shadowed nid (unused land (lnot (unused_un_bit op))) can_use_fold (depth - 1)))
 	    allowed_un;
 	  !cands in
