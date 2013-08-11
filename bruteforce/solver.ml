d5e3b0c1ea1d37c1a0741b87a0e0d6a420cc5b61
Tsutomu Ohkura <tsutomu@ohkura.com>
2013-08-11 16:08:25 +0900

    Move stuff from bruteforce_train.ml to solve.ml

---
+++ bruteforce/solver.ml
@@ -0,0 +1,96 @@
+open Remoteguess
+exception Again
+
+let get_initialguess () =
+  let bitseq = 
+    Array.to_list
+      (Array.init 64 (fun x -> Int64.shift_left 1L x)) in
+    (* let fixedseq = *)
+    (*   [0x0000000000000000L; *)
+    (*    0x0000000000000001L; *)
+    (*    0x0000000000000002L; *)
+    (*    0x0000000000000003L; *)
+    (*    0x0000000000000004L; *)
+    (*    0x0000000000000005L; *)
+    (*    0x0000000000000006L; *)
+    (*    0x0000000000000007L; *)
+    (*    0x0000000000000008L; *)
+    (*    0x0000000000000009L; *)
+    (*    0x000000000000000aL; *)
+    (*    0x000000000000000bL; *)
+    (*    0x00000000FFFFFFFFL; *)
+    (*    0xFFFFFFFF00000000L; *)
+    (*    0x0000FFFF0000FFFFL; *)
+    (*    0xFFFF0000FFFF0000L; *)
+    (*    0x00FF00FF00FF00FFL; *)
+    (*    0xFF00FF00FF00FF00L; *)
+    (*    0x0F0F0F0F0F0F0F0FL; *)
+    (*    0xF0F0F0F0F0F0F0F0L; *)
+    (*    0x3333333333333333L; *)
+    (*    0xCCCCCCCCCCCCCCCCL; *)
+    (*    0x5555555555555555L; *)
+    (*    0xAAAAAAAAAAAAAAAAL; *)
+    (*    0x0000FFFFFFFF0000L; *)
+    (*    0xFFFF00000000FFFFL; *)
+    (*    0xFF0000FFFF0000FFL; *)
+    (*    0x00FFFF0000FFFF00L; *)
+    (*    0xF00FF00FF00FF00FL; *)
+    (*    0x0FF00FF00FF00FF0L; *)
+    (*    0xDEADBEEFDEADBEEFL; *)
+    (*    0xFFFFFFFFFFFFFFFFL *)
+    (*   ] in *)
+  let randseq = 
+    Array.to_list
+      (Array.init
+         192
+           (* (256 - (List.length bitseq) - (List.length fixedseq)) *)
+	 (fun _ -> Util.rand64 ())) in
+  bitseq @ randseq
+(* bitseq @ fixedseq @ randseq in *)
+
+let solve core_problem =
+  let id, size, (unops, binops, statements) = core_problem in
+  let alllist = Brute.get_candidates core_problem in
+  if (List.length alllist) > 100000000 then
+    prerr_endline "Abandoned: too many candidates."
+  else
+  let _ = prerr_endline "Requesting initial guess..." in
+  let initialguess = get_initialguess () in
+  let start_time = Sys.time() in
+  let (status, outputs, message) = Remote.eval_id id initialguess in
+  if status <> Remoteeval.EvalStatusOk then
+    begin
+      Printf.printf "Status: \"%s\"\n" (Remoteeval.print_eval_status status);
+      failwith "Eval returned error"
+    end
+  else
+    let rec guess_function x c =
+      Printf.eprintf
+        "Guessed: %s\n"
+        (Print.print x);
+      let guess_submit () = 
+	let (status, values, message, _lightning) = Remote.guess id (Print.print x) in
+	print_endline message;
+	match status with
+	    GuessStatusWin -> Feedback.Success
+	  | GuessStatusMismatch -> 
+	    begin
+	      match values with
+		  [input; output; _youroutput] -> 
+		    Feedback.Fail (input, output)
+		| _ -> failwith "Should not happen: not three values"
+	    end
+	  | GuessStatusError -> raise Again
+	  | st -> failwith "Unknown status."
+      in
+      try guess_submit ()
+      with Again -> (Unix.sleep 20; guess_function x c)
+    in
+    let _ = GuessCaller.guess_call (List.combine initialguess outputs)
+      guess_function
+      (unops, binops, statements)
+      size
+      alllist
+    in
+    let end_time = Sys.time() in
+    Printf.eprintf "Execution time: %fs\n" (end_time -. start_time)
