0d2382f9ccc40ad3c4537722ed76b7b0c0aa0db2
Tsutomu Ohkura <tsutomu@ohkura.com>
2013-08-11 16:10:27 +0900

    Remove unnecessary code in bruteforce_train.ml

--- bruteforce/bruteforce_train.ml
+++ bruteforce/bruteforce_train.ml
@@ -1,24 +1,3 @@
-open Remoteeval
-
-open Util
-open ExtList
-
-let main =
-  if false then
-    let bitseq = 
-      Array.to_list
-	(Array.init 64 (fun x -> Int64.shift_left 1L x)) in
-    let syntax = 
-      let open Type in
-	  Fold (Input, Zero, 0, 1, Op2 (And, Ident 0, Op1 (Not, Ident 1))) in
-      let outputs = List.map (Eval.eval syntax) bitseq in
-      let merge = List.combine bitseq outputs in
-      List.iter
-	(fun (i,o) -> Printf.printf "%LX => %LX\n" i o)
-	merge
-  else
-    ()
-
 let main =
   let _ = Random.self_init() in
   let core_problem = Remote.fetch_one_core_problem 10 "" "train" in
